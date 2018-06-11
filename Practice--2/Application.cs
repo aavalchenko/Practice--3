565ac19afa6b51eda8272d9e09ba4e7f570ea1ff
Антон Вальченко <aavalchenko@edu.hse.ru>
2018-06-11 23:31:41 +0500

    Задание решено

---
+++ Practice--2/Application.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using InputOutputLib;
+
+namespace Practice__2
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Значение значение функции в заданной точке: {0}.", Function(Get.Double("Введите число a: ")));
+            Console.ReadKey();
+        }
+
+        private static double Function(Double x)
+        {
+            if (x <= 0)
+                return Math.Abs(x + 1);
+
+            return Math.Abs(x - 1);
+        }
+    }
+}
