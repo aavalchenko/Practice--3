2ae087219fbad8b6c77667c132608b69b469e8a5
Антон Вальченко <aavalchenko@edu.hse.ru>
2018-06-23 13:30:44 +0500

    Code review

--- Practice--3/Application.cs
+++ Practice--3/Application.cs
@@ -3,14 +3,43 @@ using InputOutputLib;
 
 namespace Practice__3
 {
-    class Application
+    sealed internal class Application
     {
+        private static string[] menuStrings = { "Программа вычисления функции y = | |a| - 1 |:", "Задать значение a"}; // Пункты меню
+        private static Menu mainMenu = new Menu(menuStrings);                                                          // Создание нового меню с заданными пунктами
+        private static bool status = true;
         static void Main(string[] args)
         {
-            Console.WriteLine("Значение значение функции в заданной точке: {0}.", Function(Get.Double("Введите число a: ")));
-            Console.ReadKey();
+            while(status)
+            {
+                switch (mainMenu.Show())
+                {
+                    case ConsoleKey.NumPad1:
+                    case ConsoleKey.D1:
+                        Console.Clear();
+                        Console.WriteLine("Значение функции в заданной точке: {0}.",
+                            Function(Get.Double("Введите число a: ")));
+                        Get.Wait();
+                        break;
+
+                    case ConsoleKey.Escape:
+                        status = false;
+                        break;
+                    
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Выбран несуществующий пункт меню");
+                        Get.Wait();
+                        break;
+                }
+            }
         }
 
+        /// <summary>
+        /// Вычисление значения функции в заданной точке.
+        /// </summary>
+        /// <param name="x">Точка для вычисления.</param>
+        /// <returns></returns>
         private static double Function(Double x)
         {
             if (x <= 0)
